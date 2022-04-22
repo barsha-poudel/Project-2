049ccc672a4154deefd68199efee9a85b2c94de2
barsha-poudel <48411297+barsha-poudel@users.noreply.github.com>
2022-04-22 16:18:16 +0545

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,25 @@
 function Card(props){
+    let badgeText
+    if(props.item.openSpots === 0){
+        badgeText = "SOLDOUT"
+    }else if (props.item.location === "Online"){
+        badgeText = "ONLINE"
+    }
+
+
     return(
         <div className="card">
+                {badgeText && <div className="card-badge">{badgeText}</div>}
             <div className="card-img"></div>
-            <img src={`../images/${props.img}`}/>
+            <img src={`../images/${props.item.coverImg}`}/>
             <div className="card--stats">
                 <img src="../images/star.png" className="card--star"/>
-                <span>{props.rating}</span>
-                <span className="gray">{props.reviewCount} </span>
-                <span className="gray">{props.country}</span>
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">({props.item.stats.reviewCount}) • </span>
+                <span className="gray">{props.item.location}</span>
             </div>
-            <p className="card-title">{props.title}</p>
-            <p className="card-price"><span className="bold">{props.price}</span> / person</p>
+            <p className="card-title">{props.item.title}</p>
+            <p className="card-price"><span className="bold">{props.item.price}</span> / person</p>
 
         </div>
     )
